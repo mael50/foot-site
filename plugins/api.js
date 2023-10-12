5967da869cfdb74120bfb3a3f323821e6f32ae04
mael <maellaroque50@gmail.com>
2023-10-12 15:24:38 +0200

    update api url (http -> https)

--- plugins/api.js
+++ plugins/api.js
@@ -6,7 +6,7 @@ export default defineNuxtPlugin(() => {});
 // Fonction pour appeler l'API
 export async function callApi(endpoint) {
     try {
-        const response = await fetch('http://footsite.maellaroque.fr/api.php?uri=' + url + endpoint);
+        const response = await fetch('https://footsite.maellaroque.fr/api.php?uri=' + url + endpoint);
         const data = await response.json();
         return data;
     } catch (error) {
